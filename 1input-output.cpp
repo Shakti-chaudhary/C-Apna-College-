0005de0bd9f2855c8ca23146151f901ce0f4f09b
Shakti.chaudhary <shaktichaudhary4321@gmail.com>
2020-12-10 14:26:54 +0530

    Initial commit

---
+++ 1input-output.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+// output:- 
+
+cout<<"here the first output"<<endl;
+char ch = 'a';
+cout<<"char:- "<<ch<<endl;
+ 
+//  ---------------------------------------------------
+
+//  input:- 
+ 
+//  char ch;
+//  cout<<"enter the first char"<<endl;
+//  cin>>ch;
+//  cout<<ch<<endl;
+
+// int x,y;
+// cout<<"enter the integer x and y:"<<endl;
+// cin>>x>>y;
+// cout<<x<<y<<endl;
+
+
+}
